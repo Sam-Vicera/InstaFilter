b55c0f4a69dfe4564ecc9bf763353a761d74d3e1
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-04-11 11:41:45 -0700

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Samuel Hernandez Vicera on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
